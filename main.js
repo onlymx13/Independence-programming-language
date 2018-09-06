49a64c0fef19ef880ff2567bd885f46b9dc2f179
onlymx13 <onlymx13@gmail.com>
2018-09-06 09:29:06 -0500

    Add splitting reflex

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+'use strict';
+var code, sentences;
 document.getElementsByTagName('button')[0].onClick = function () {
-
+    code = document.getElementsByTagName('textArea')[0].value;
+    sentences = code.split(/!./);
 }
