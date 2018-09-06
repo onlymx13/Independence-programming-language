aaee52fafb4be1cc9789f6bbe88e2d7af782389d
onlymx13 <onlymx13@gmail.com>
2018-09-06 12:02:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 'use strict';
-var code, sentences;
+var text, sentences, code;
+function throwError(error) {
+}
 document.addEventListener('DOMContentLoaded') {
     document.getElementsByTagName('button')[0].onClick = function () {
-        code = document.getElementsByTagName('textArea')[0].value;
-        sentences = code.split(".");
+        text = document.getElementsByTagName('textArea')[0].value;
+        sentences = text.split(".");
+        if (!/The unanimous Declaration of the (zero|one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen) united States of America/.test(sentences[0])) throwError("Invalid declaration of 'independence'");
         sentences.forEach (
             function (element) {
                 
