d8bb3beeff97a9f8bf848e9500f26577430522e8
onlymx13 <onlymx13@gmail.com>
2018-09-06 10:10:37 -0500

    Fix regex

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ var code, sentences;
 document.addEventListener('DOMContentLoaded') {
     document.getElementsByTagName('button')[0].onClick = function () {
         code = document.getElementsByTagName('textArea')[0].value;
-        sentences = code.split(/!./);
+        sentences = code.split(".");
     }
 }
