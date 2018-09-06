b6a475a6e23d5f99695cd84048c8eeeeee581f92
onlymx13 <onlymx13@gmail.com>
2018-09-06 16:44:47 -0500

    typo

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function throwError(error) {
     alert("Error: "+error);
 }
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementsByTagName('button')[0].onClick = function () {
+    document.getElementsByTagName('button')[0].onclick = function () {
         text = document.getElementsByTagName('textArea')[0].value;
         sentences = text.split(".");
         if (!/The unanimous Declaration of the (zero|one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen) united States? of America/.test(sentences[0])) throwError("Invalid declaration of 'independence'");
