00d67818718123cdee55810132f941273d04315f
onlymx13 <onlymx13@gmail.com>
2018-09-06 13:47:22 -0500

    Improve error throwing

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 'use strict';
 var text, sentences, code;
 function throwError(error) {
-    alert(error);
+    alert("Error: "+error);
 }
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementsByTagName('button')[0].onClick = function () {
         text = document.getElementsByTagName('textArea')[0].value;
         sentences = text.split(".");
-        if (!/The unanimous Declaration of the (zero|one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen) united States of America/.test(sentences[0])) throwError("Invalid declaration of 'independence'");
+        if (!/The unanimous Declaration of the (zero|one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen) united States? of America/.test(sentences[0])) throwError("Invalid declaration of 'independence'");
         sentences.forEach (
             function (element) {
                 
