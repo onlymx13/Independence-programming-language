31e7f6284e60e98742e340d687a0ca303d95ad3c
onlymx13 <onlymx13@gmail.com>
2018-09-24 20:32:42 -0500

    fix the same typo again. who knew?

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (!/The unanimous Declaration of the (zero|one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen) united States? of America/.test(sentences[0])){
             throwError("Error: Invalid declaration of 'independence'");
         }
-        if (!sentences.includes("These united Colonies are, and of Right ought to be, Free and Independent States")) {
+        if (!sentences.includes("These united Colonies are, and of Right ought to be Free and Independent States")) {
             throwError("Error: No declaration that these united Colonies are Free and Independent States");
         }
         while (!endFlag) {
