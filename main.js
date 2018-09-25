ac28ffa9ef5b019a649956cc09f4852a9d67bfcd
onlymx13 <onlymx13@gmail.com>
2018-09-24 20:58:20 -0500

    fix constants

--- main.js
+++ main.js
@@ -12,18 +12,18 @@ function constant(number) {
         throwError("Error: constant not beginning with 'certain unalienable rights, such as'");
     }
     number = number.slice(35);
-    number = number.replace("Life",1);
-    number = number.replace("Liberty",2);
-    number = number.replace("Pursuit of Happiness",5);
-    number = number.replace("Property",10);
-    number = number.replace(", and","+");
-    number = number.replace(" and","+");
-    number = number.replace(", ",'+');
-    number = number.replace("no ","-1*");
-    number = number.replace("lack of ","-1*");
-    number = number.replace("good ","2*");
-    number = number.replace("great ","4*");
-    return number;
+    number = number.replace(/Life/g,1);
+    number = number.replace(/Liberty/g,2);
+    number = number.replace(/Pursuit of Happiness/g,5);
+    number = number.replace(/Property/g,10);
+    number = number.replace(/, and/g,"+");
+    number = number.replace(/ and/g,"+");
+    number = number.replace(/, /g,'+');
+    number = number.replace(/no /g,"-1*");
+    number = number.replace(/lack of /g,"-1*");
+    number = number.replace(/good /g,"2*");
+    number = number.replace(/great /g,"4*");
+    return Function(number);
 }
 function run(line) {
     if (line === "These united Colonies are, and of Right ought to be Free and Independent States") {
